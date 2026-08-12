06bedfda4820cc5971ba31890c310fc5b0a466ea
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2026-08-12 16:32:12 +0500

    Update API URLs for production

--- jira-clone/frontend/src/api/axios.js
+++ jira-clone/frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://jira-api.umais.online/api',
 });
 
 api.interceptors.request.use((config) => {
