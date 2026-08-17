a2729d7d0351071c29185cc0d9aa4d7e1e64ec3f
Abdul Mohaiman <abdulmohaiman333@gmail.com>
2026-08-17 14:32:53 +0500

    Chnages URL's

--- chatsphere/frontend/src/api/axios.js
+++ chatsphere/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://chatapp1-hpaik4df.b4a.run/api',
 });
 
 API.interceptors.request.use((req) => {
--- chatsphere/frontend/src/pages/Chat.jsx
+++ chatsphere/frontend/src/pages/Chat.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 import API from '../api/axios';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://chatapp1-hpaik4df.b4a.run');
 
 export default function Chat() {
   const { user, logout } = useAuth();
